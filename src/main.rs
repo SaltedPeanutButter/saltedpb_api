6c28437b8a66c2c4be31fe43859f112a32d6989a
Nguyen Thai Binh <binhnt.dev@icloud.com>
2023-09-17 16:23:32 +0800

    Add temporary endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get, Server};
+
+#[tokio::main]
+async fn main() {
+    // Initialise tracing
+    tracing_subscriber::fmt::init();
+
+    // Build test app
+    let app = Router::new()
+        .route("/", get(|| async {
+            "Deployment successful!"
+        }));
+
+    // Run app
+    Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
